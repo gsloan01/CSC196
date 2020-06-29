629b78294063d14031c45c86407d5d421b3eb591
gsloan01 <60530127+gsloan01@users.noreply.github.com>
2020-06-29 16:48:54 -0600

    Commit Uno

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,20 +1,24 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+#include "core.h" 
 
-int main()
-{
-    std::cout << "Hello World!\n";
+bool Update(float dt) 
+{ 
+	return false;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void Draw(Core::Graphics& graphics)
+{
+	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
+	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+int main() 
+{
+	char name[] = "CSC196";
+	Core::Init(name, 800, 600);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw); 
+	Core::GameLoop(); Core::Shutdown(); 
+}
